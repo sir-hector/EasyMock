b2b574dfe34563153b97ac8c8d0a6ecdfb27d29e
sir-hector <krauskarol7@gmail.com>
2023-01-15 08:52:31 +0100

    add test for race using mock

--- src/test/java/MockExamples/MockExamples/RaceResultServiceEasyMockTest.java
+++ src/test/java/MockExamples/MockExamples/RaceResultServiceEasyMockTest.java
@@ -11,19 +11,6 @@ import org.junit.Test;
 public class RaceResultServiceEasyMockTest {
 
     @Test
-    public void testAddSubscriber() {
-        Client client = createMock(Client.class);
-        RaceResultService service = new RaceResultService();
-
-        client.receive(anyObject(Message.class));
-        expectLastCall().once();
-        replay(client);
-
-        service.addSubscriber(client);
-        verify(client);
-    }
-
-    @Test
     public void testSend() {
         Client client1 = createMock(Client.class);
         Client client2 = createMock(Client.class);
@@ -45,21 +32,4 @@ public class RaceResultServiceEasyMockTest {
         verify(client2);
     }
 
-    @Test
-    public void testRemoveSubscriber() {
-        Client client1 = createMock(Client.class);
-        Client client2 = createMock(Client.class);
-        Message message = createMock(Message.class);
-        RaceResultService service = new RaceResultService();
-
-        replay(client1, client2);
-
-        service.addSubscriber(client1);
-        service.addSubscriber(client2);
-        service.removeSubscriber(client1);
-        service.send(message);
-
-        verify(client1, never()).receive(message);
-        verify(client2).receive(message);
-    }
 }
